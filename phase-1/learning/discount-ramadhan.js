c59693bbc677cdf34d26faa3648978fd9bb6f577
saepulfariz <saepulfariz108@gmail.com>
2025-05-12 05:34:19 +0700

    add function discount ramadhan

---
+++ phase-1/learning/discount-ramadhan.js
@@ -0,0 +1,16 @@
+function run(n) {
+  var discount = 0;
+  if (n > 500000) {
+    discount = 15;
+  } else if (n > 35000) {
+    discount = 10;
+  } else if (n > 20000) {
+    discount = 5;
+  }
+
+  var price_discount = (n * discount) / 100;
+  var price_total = n - price_discount;
+  return Math.ceil(price_total);
+}
+
+module.exports = { run };
