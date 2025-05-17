1f15e087ea183b20d6ec69832b6c399ae930d96f
saepulfariz <saepulfariz108@gmail.com>
2025-05-17 19:09:33 +0700

    add test learning palindrome #2-5

--- phase-1/learning/learning-palindrome.test.js
+++ phase-1/learning/learning-palindrome.test.js
@@ -5,3 +5,27 @@ test("Learning Palindrome - #1", () => {
   var result = "benar";
   expect(run(n)).toBe(result);
 });
+
+test("Learning Palindrome - #2", () => {
+  var n = "12/02/2021";
+  var result = "benar";
+  expect(run(n)).toBe(result);
+});
+
+test("Learning Palindrome - #3", () => {
+  var n = "jabar";
+  var result = "salah";
+  expect(run(n)).toBe(result);
+});
+
+test("Learning Palindrome - #4", () => {
+  var n = "dont’t nod";
+  var result = "benar";
+  expect(run(n)).toBe(result);
+});
+
+test("Learning Palindrome - #5", () => {
+  var n = "forwards";
+  var result = "salah";
+  expect(run(n)).toBe(result);
+});
